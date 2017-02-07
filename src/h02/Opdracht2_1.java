2aa7a47306a069a741fee5ce36f5f903228dc902
anwar <2003500@talnet.nl>
2017-02-07 09:15:18 +0100

    Opdracht 2_2

---
+++ src/h02/Opdracht2_1.java
@@ -0,0 +1,20 @@
+package h02;
+
+import java.awt.*;
+import java.applet.*;
+
+//een klasse met de naam Show van het type Applet
+
+public class Opdracht2_1 extends Applet {
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Anwar", 50, 60 );
+    }
+}
+
+
