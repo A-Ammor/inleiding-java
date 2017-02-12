f52bb11fa8e9a6bef60dfc466544eee4837a3856
anwar <2003500@talnet.nl>
2017-02-12 23:55:20 +0100

    Opdracht 4_1 Opdracht 5_1 en 5_2 erbij gedaan. Opdracht 6 t/m praktijk erbij gedaan.

---
+++ src/h06/Praktijkopdracht_h06.java
@@ -0,0 +1,37 @@
+package h06;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by anwar on 12-2-2017.
+ */
+
+public class Praktijkopdracht_h06 extends Applet {
+    double uitkomst, cijfer1, cijfer2, cijfer3, alleCijfers;
+    int gemiddelde;
+    //andere double gemaakt omdat ik dat makkelijker vond.
+    double uitkomstDouble, antwoordDouble;
+    int test2;
+
+
+    public void init() {
+        test2 = 0;
+        cijfer1 = 5.9;
+        cijfer2 = 6.3;
+        cijfer3 = 6.9;
+        alleCijfers = 3;
+        uitkomstDouble = gemiddelde;
+
+        // uitkomst double is dat het van int naar double gaat en antwoord double is dat het antwoord met een komma is
+        uitkomst = (cijfer1 + cijfer2 + cijfer3) / alleCijfers * 10;
+        gemiddelde = (int) uitkomst;
+        uitkomstDouble = gemiddelde;
+        antwoordDouble = uitkomstDouble / 10;
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("De uitkomst is: " + antwoordDouble + " easy", 20, 20);
+    }
+}
