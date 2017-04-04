d9bf5066fe9d0f907c3c4191f717c81b3d1b7275
anwar <2003500@talnet.nl>
2017-04-04 13:19:46 +0200

    Toets 1

--- src/Toetsen/Toets1.java
+++ src/Toetsen/Toets1.java
@@ -28,7 +28,6 @@ public class Toets1 extends Applet {
         add(button);
     }
 
-
     public void paint(Graphics g) {
         if (click) {
             g.drawString("Tijd in Canada: " + tijdInCanada + ":00 uur", 100, 100);
@@ -40,7 +39,6 @@ public class Toets1 extends Applet {
             g.drawString("voer geldige invoer in", 100, 180);
         }
     }
-
 
     class Listener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
@@ -56,7 +54,6 @@ public class Toets1 extends Applet {
             if (tijdInCanada > 24) {
                 tijdInCanada = tijdInCanada - 24;
             }
-
             tijdInChina = invoer + 8;
             if (invoer >= 0 && invoer <= 24) {
                 getGraphics().drawString("", 70, 140);
@@ -67,7 +64,6 @@ public class Toets1 extends Applet {
             if (tijdInChina < 0) {
                 tijdInChina = tijdInChina + 24;
             }
-
             tijdInBrazilie = invoer - 3;
             if (invoer >= 0 && invoer <= 24) {
                 getGraphics().drawString("", 70, 140);
@@ -78,7 +74,6 @@ public class Toets1 extends Applet {
             if (tijdInBrazilie < 0) {
                 tijdInBrazilie = tijdInBrazilie + 24;
             }
-
             tijdInAustralie = invoer + 9;
             if (tijdInAustralie > 24) {
                 tijdInAustralie = tijdInAustralie - 24;
@@ -90,18 +85,7 @@ public class Toets1 extends Applet {
                 getGraphics().drawString("voer geldig invoer", 70, 100);
             }
         }
-
-        class KnopListener implements ActionListener {
-            public void actionPerformed(ActionEvent e) {
-                if (!click) {
-                    click = true;
-                }
-            }
-
-        }
-
     }
-
     class KnopListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             if (!click) {
@@ -110,5 +94,3 @@ public class Toets1 extends Applet {
         }
     }
 }
-
-
