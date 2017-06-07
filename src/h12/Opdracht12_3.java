a4d6c5ed115ded33a2bcd30e063f6786350ffa11
anwar <2003500@talnet.nl>
2017-06-07 11:30:29 +0200

    Hoofdstuk 11 toegevoegd

---
+++ src/h12/Opdracht12_3.java
@@ -0,0 +1,51 @@
+package h12;
+
+/**
+ * Created by anwar on 6-6-2017.
+ */
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+public class Opdracht12_3 extends Applet {
+    TextField[] textfield;
+    int[] getallen;
+    Button b;
+
+
+    public void init() {
+        setSize(800, 200);
+        textfield = new TextField[5];
+        getallen = new int[5];
+
+
+        for (int i = 0; i < textfield.length; i++) {
+            textfield[i]= new TextField("", 10);
+            add(textfield[i]);
+        }
+
+        b = new Button("klik op mij");
+        add(b);
+        b.addActionListener(new KnopListener());
+    }
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+            for (int i = 0; i < textfield.length; i++) {
+                getallen[i] = Integer.parseInt(textfield[i].getText());
+            }
+
+            Arrays.sort(getallen);
+
+            for (int i = 0; i < getallen.length; i++) {
+                textfield[i].setText(String.valueOf(getallen[i]));
+            }
+        }
+    }
+}
+
+
+
+
